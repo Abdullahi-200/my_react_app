46ac1726d0084c1c4e25da43f2e0db038558c293
Abdullahi-200 <103432923+Abdullahi-200@users.noreply.github.com>
2022-09-19 18:26:10 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import Calculators from "./Components/calculator/Calculators";
+
+
+//6c08b75f
+
+function App () {
+ 
+    return (
+      <div className = 'App'> 
+      
+      <Calculators/>
+        </div> 
+    );
+}
+
+
+
+export  default App
